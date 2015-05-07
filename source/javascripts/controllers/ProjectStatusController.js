4eeaea9b93695ab46ffd4ae2bdcd146afa09e74b
william-index <thewilliamanderson@gmail.com>
2015-05-07 12:26:11 -0400

    reveal and removal of detail view on click

--- source/javascripts/controllers/ProjectStatusController.js
+++ source/javascripts/controllers/ProjectStatusController.js
@@ -1,6 +1,11 @@
+/*
+* Main Controller for the application
+* @class ProjectStatusController
+*/
 app.controller('ProjectStatusController', function($scope, $http) {
 
-  /* retrieves data from json. Data folder not used as
+  /*
+    retrieves data from json. Data folder not used as
     it has a convention with middlmean, so I named the dir "json"
     for distinction
   */
@@ -23,8 +28,31 @@ app.controller('ProjectStatusController', function($scope, $http) {
   // currently displayed details
   $scope.selectedProject = {};
 
+  /**
+  * Updates the active project and appends
+  *   class for active state of project details
+  *   lightbox view
+  * @method showDetails
+  * @memberof ProjectStatusController
+  *
+  * @param {object} project
+  **/
   $scope.showDetails = function(project){
+    var _detail_view = angular.element(document.getElementById("project_details"));
+    _detail_view.addClass("project_details--active")
     $scope.selectedProject = project;
   };
+
+  /**
+  * Simple removes the active class from the
+  *   detail view allow it to animate out
+  *   based on css
+  * @method hideDetails
+  * @memberof ProjectStatusController
+  **/
+  $scope.hideDetails = function(){
+    var _detail_view = angular.element(document.getElementById("project_details"));
+    _detail_view.removeClass("project_details--active")
+  }
 
 });
